24c80d031e15dd9ec8c8ff27e3f6c128c761013b
Thomas Chen <tom.chen@am.sony.com>
2017-04-30 12:16:58 -0700

    initial commit

---
+++ tests/unit/utils/require-thunk-test.js
@@ -0,0 +1,10 @@
+import thunk from 'redux-thunk';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | require thunk');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  assert.ok(thunk);
+  assert.equal(typeof thunk, 'function');
+});
